6fdd02454745d8383de1d1017c36a6791bf8e2cf
RistoFlink <kenssi@gmail.com>
2024-01-02 21:15:32 +0200

    Test WaitGroups

--- first-example/main.go
+++ first-example/main.go
@@ -2,19 +2,40 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
 // Technically every program in Go has a GoRoutine since the main-function is itself one
 // GoRoutines are very lightweight threads
 func main() {
-	go printSomething("This is the first thing to be printed!")
+	var wg sync.WaitGroup
 
-	time.Sleep(1 * time.Second)
+	words := []string{
+		"alpha",
+		"beta",
+		"delta",
+		"gamma",
+		"pi",
+		"zeta",
+		"eta",
+		"theta",
+		"epsilon",
+	}
 
-	printSomething("This is the second thing to be printed!")
+	wg.Add(len(words))
+
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, x), &wg)
+	}
+
+	wg.Wait()
+
+	wg.Add(1)
+	printSomething("This is the second thing to be printed!", &wg)
 }
 
-func printSomething(s string) {
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	fmt.Println(s)
 }
