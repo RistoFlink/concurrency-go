5f0fe95cdf720c52ff07ca7d9ddc0199af2cfb13
RistoFlink <kenssi@gmail.com>
2025-01-19 23:02:44 +0200

    Implement the Producer

--- producer-consumer/main.go
+++ producer-consumer/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"math/rand"
+	"time"
+)
+
 const NumberOfPizzas = 10
 
 var pizzasMade, pizzasFailed, total int
@@ -15,15 +22,84 @@ type PizzaOrder struct {
 	success     bool
 }
 
-func main() {
-	// seed the RNG
+func (p *Producer) Close() error {
+	ch := make(chan error)
+	p.quit <- ch
+	return <-ch
+}
 
+func makePizza(pizzaNumber int) *PizzaOrder {
+	pizzaNumber++
+	if pizzaNumber <= NumberOfPizzas {
+		delay := rand.Intn(5) + 1
+		fmt.Printf("Received order #%d\n", pizzaNumber)
+
+		rnd := rand.Intn(12) + 1
+		msg := ""
+		success := false
+
+		if rnd < 5 {
+			pizzasFailed++
+		} else {
+			pizzasMade++
+		}
+		total++
+
+		fmt.Printf("Preparing pizza #%d. It will take %d seconds\n", pizzaNumber, delay)
+		// delay for a bit..
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2 {
+			msg = fmt.Sprintf("*** We ran out of ingredients for pizza #%d\n", pizzaNumber)
+		} else if rnd <= 4 {
+			msg = fmt.Sprintf("*** We accidentally burned the #%d\n", pizzaNumber)
+		} else {
+			success = true
+			msg = fmt.Sprintf("Pizza #%d is ready!\n", pizzaNumber)
+		}
+		p := &PizzaOrder{pizzaNumber, msg, success}
+		return p
+	}
+	return &PizzaOrder{pizzaNumber: pizzaNumber}
+}
+
+func pizzeria(pizzaMaker *Producer) {
+	// keep track of what pizza is being made
+	var i = 0
+	// run forever or until we receive a quit notification
+
+	// try to make pizzas
+	for {
+		currentPizza := makePizza(i)
+		if currentPizza != nil {
+			i = currentPizza.pizzaNumber
+			select {
+			// we tried to make a pizza (we sent something to the data channel)
+			case pizzaMaker.data <- *currentPizza:
+			case quitChan := <-pizzaMaker.quit:
+				// close channels
+				close(pizzaMaker.data)
+				close(quitChan)
+				return
+			}
+		}
+	}
+}
+
+func main() {
+	// seed the RNG - not needed since go 1.20?
 	// print the start message
+	color.Green("Risto's pizzeria is open for business")
+	color.Cyan("--------------------------------------")
 
 	// create a producer
+	pizzaJob := &Producer{
+		data: make(chan PizzaOrder),
+		quit: make(chan chan error),
+	}
 
 	// run the producer in the background
-
+	go pizzeria(pizzaJob)
 	// create and run the consumer
 
 	// print the end message
