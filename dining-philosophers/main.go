2fd765db83d15fdc6dc036a385046d764be5bcf2
RistoFlink <kenssi@gmail.com>
2025-01-20 19:31:38 +0200

    Implement the logic for diningProblem()

--- dining-philosophers/main.go
+++ dining-philosophers/main.go
@@ -42,6 +42,11 @@ func main() {
 }
 
 func dine() {
+	// only for development for speeding up the process
+	eatTime = 0 * time.Second
+	thinkTime = 0 * time.Second
+	sleepTime = 0 * time.Second
+
 	wg := &sync.WaitGroup{}
 	wg.Add(len(philosophers))
 
@@ -65,4 +70,40 @@ func dine() {
 
 func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*sync.Mutex, seated *sync.WaitGroup) {
 	defer wg.Done()
+
+	// seat the philosopher
+	fmt.Printf("%s is seated at the table.\n", philosopher.name)
+	seated.Done()
+
+	seated.Wait()
+
+	// eat three time
+	for i := hunger; i > 0; i-- {
+		// get a lock on both forks
+		if philosopher.leftFork > philosopher.rightFork {
+			forks[philosopher.rightFork].Lock()
+			fmt.Printf("\t%s takes the right fork.\n", philosopher.name)
+			forks[philosopher.leftFork].Lock()
+			fmt.Printf("\t%s takes the left fork.\n", philosopher.name)
+		} else {
+			forks[philosopher.leftFork].Lock()
+			fmt.Printf("\t%s takes the left fork.\n", philosopher.name)
+			forks[philosopher.rightFork].Lock()
+			fmt.Printf("\t%s takes the right fork.\n", philosopher.name)
+		}
+
+		fmt.Printf("\t%s has both forks and is eating.\n", philosopher.name)
+		time.Sleep(eatTime)
+
+		fmt.Printf("\t%s is thinking.\n", philosopher.name)
+		time.Sleep(thinkTime)
+
+		forks[philosopher.leftFork].Unlock()
+		forks[philosopher.rightFork].Unlock()
+
+		fmt.Printf("\t%s has put down the forks.\n", philosopher.name)
+	}
+
+	fmt.Println(philosopher.name, "is satisfied.")
+	fmt.Println(philosopher.name, "left the table.")
 }
