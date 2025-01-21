6698de6a514957b6c918c7f6737b0d6baf15a89b
RistoFlink <kenssi@gmail.com>
2025-01-21 13:33:40 +0200

    Add some variables and struct for the barber shop

--- sleeping-barber/main.go
+++ sleeping-barber/main.go
@@ -1,15 +1,39 @@
 package main
 
+import (
+	"time"
+
+	"github.com/fatih/color"
+)
+
 // variables
+var (
+	seatingCapacity = 10
+	arrivalRate     = 100
+	cutDuration     = 1000 * time.Millisecond
+	timeOpen        = 10 * time.Second
+)
 
 func main() {
-	// seed ?
-
 	// print welcome message
+	color.Yellow("The Sleeping Barber Problem")
+	color.Yellow("---------------------------")
 
 	// create channels if needed
+	clientChan := make(chan string, seatingCapacity)
+	doneChan := make(chan bool)
 
 	// create the barber shop
+	shop := BarberShop{
+		ShopCapacity:    seatingCapacity,
+		HaircutDuration: cutDuration,
+		NumberOfBarbers: 0,
+		ClientsChan:     clientChan,
+		BarbersDoneChan: doneChan,
+		Open:            true,
+	}
+
+	color.Green("The shop is open for business!")
 
 	// add barbers
 
