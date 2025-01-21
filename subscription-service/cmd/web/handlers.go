68839dab597acc68c14ea8f835c9cc2d85e52e97
RistoFlink <kenssi@gmail.com>
2025-01-21 22:40:54 +0200

    Add models, queries and login/logout functions

--- subscription-service/cmd/web/handlers.go
+++ subscription-service/cmd/web/handlers.go
@@ -10,14 +10,76 @@ func (app *Config) LoginPage(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, "login.page.gohtml", nil)
 }
 
-func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {}
+func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
+	_ = app.Session.RenewToken(r.Context())
 
-func (app *Config) Logout(w http.ResponseWriter, r *http.Request) {}
+	// parse form post
+	err := r.ParseForm()
+	if err != nil {
+		app.ErrorLog.Println(err)
+	}
+
+	// get email and password from form post
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+
+	user, err := app.Models.User.GetByEmail(email)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Invalid credentials.")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	// check the password
+	validPassword, err := user.PasswordMatches(password)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Invalid credentials.")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	if !validPassword {
+		app.Session.Put(r.Context(), "error", "Invalid credentials.")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	// log the user
+	app.Session.Put(r.Context(), "userID", user.ID)
+	app.Session.Put(r.Context(), "user", user)
+
+	app.Session.Put(r.Context(), "flash", "Logged in successfully!")
+	// redirect the user
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+
+}
+
+func (app *Config) Logout(w http.ResponseWriter, r *http.Request) {
+	// delete the session
+	_ = app.Session.Destroy(r.Context())
+	_ = app.Session.RenewToken(r.Context())
+
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+}
 
 func (app *Config) RegisterPage(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, "register.page.gohtml", nil)
 }
 
-func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {}
+func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
+	// create a user
 
-func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {}
+	// send an activation email
+
+	// subscribe the user to an account
+}
+
+func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
+	// validate the url
+
+	// generate an invoice
+
+	// send an email with attachments
+
+	// send an email with the invoice
+}
--- subscription-service/cmd/web/main.go
+++ subscription-service/cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/gob"
 	"fmt"
 	"github.com/alexedwards/scs/redisstore"
 	"github.com/alexedwards/scs/v2"
@@ -13,6 +14,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"subscription-service/data"
 	"sync"
 	"syscall"
 	"time"
@@ -43,6 +45,7 @@ func main() {
 		InfoLog:  infoLog,
 		ErrorLog: errorLog,
 		Wait:     &wg,
+		Models:   data.New(db),
 	}
 
 	// setup email sending
@@ -118,6 +121,7 @@ func openDB(dsn string) (*sql.DB, error) {
 }
 
 func initSession() *scs.SessionManager {
+	gob.Register(data.User{})
 	// set up session
 	session := scs.New()
 	session.Store = redisstore.New(initRedis())
